9f75cb5829353a3ca9d3fd05d60183150539978a
rider <riderjensen@gmail.com>
2020-04-06 19:35:59 -0600

    Changing files to read json

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import flask
 from flask import request, jsonify
-import sqlite3
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
 @app.route('/', methods=['GET'])
@@ -20,51 +19,30 @@ def home():
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM books;').fetchall()
 
-    return jsonify(all_books)
+    with open('books.json') as json_file:
+        data = json.load(json_file)
 
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    return jsonify(data)
 
 
 @app.route('/api/v1/resources/books', methods=['GET'])
 def api_filter():
     query_parameters = request.args
 
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
+    name = query_parameters.get('name')
+    age = query_parameters.get('age')
 
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
+    data = {}
+    returnItem = []
 
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
+    with open('books.json') as json_file:
+        data = json.load(json_file)
 
-    query = query[:-4] + ';'
+    for p in data['people']:
+        if p['age'] == int(age):
+            returnItem.append(p)
 
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-
-    results = cur.execute(query, to_filter).fetchall()
-
-    return jsonify(results)
+    return jsonify(returnItem)
 
 app.run()
